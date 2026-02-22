1fc30a8deee9e72f4ea33fa7b09b13050c8f971f
Ezra Wolf <wolfez@mail.uc.edu>
2026-02-22 00:41:03 -0500

    Implement TinyQV multiplier

--- src/cpu/alu.v
+++ src/cpu/alu.v
@@ -27,9 +27,17 @@ module tinymoa_multiplier #(parameter B_IN_WIDTH = 16;) (
 
     input [3:0] a_in,
     input [B_IN_WIDTH-1:0] b_in,
-    output [3:0] result
+
+    output [3:0] product
 );
-    // TODO
+    reg [B_IN_WIDTH-1:0] accumulator;
+    wire [B_IN_WIDTH+3:0] next_accumulator = {4'b0, accumulator} + {{B_IN_WIDTH{1'b0}}, a_in} * {4'd0, b_in};
+
+    always @(posedge clk) begin
+        accumulator <= (a_in != 4'b0000) ? next_accumulator[B_IN_WIDTH+3:4] : {4'b0000, accumulator[B_IN_WIDTH-1:4]};
+    end
+
+    assign product = next_accumulator[3:0];
 endmodule
 
 
