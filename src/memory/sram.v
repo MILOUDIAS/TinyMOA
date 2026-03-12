857d697345e8f8ad26d2ae6e1004b06cac72c138
Ezra Wolf <wolfez@mail.uc.edu>
2026-03-12 13:28:07 -0400

    Fix SRAM pinouts

--- src/memory/sram.v
+++ src/memory/sram.v
@@ -7,19 +7,19 @@ module SG13G2_512x32_SP (
     input A_MEN,
     input A_WEN,
     input A_REN,
-    input [9:0] A_ADDR,
-    input [7:0] A_DIN,
+    input [8:0] A_ADDR,
+    input [31:0] A_DIN,
     input A_DLY,
-    output [7:0] A_DOUT,
-    input [7:0] A_BM,
+    output [31:0] A_DOUT,
+    input [31:0] A_BM,
     input A_BIST_CLK,
     input A_BIST_EN,
     input A_BIST_MEN,
     input A_BIST_WEN,
     input A_BIST_REN,
-    input [9:0] A_BIST_ADDR,
-    input [7:0] A_BIST_DIN,
-    input [7:0] A_BIST_BM
+    input [8:0] A_BIST_ADDR,
+    input [31:0] A_BIST_DIN,
+    input [31:0] A_BIST_BM
 );
-    // LEF
+    // LEF from `/macro/SG13G2_512x32_SP.lef`
 endmodule
